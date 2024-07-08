5aa746d42227c2b896793779e7cdbdb5e305138c
sanyam <sanyammadaanfca@gmail.com>
2024-07-08 14:56:47 +0530

    triall

--- frontend/Paytm/src/Components/Heading.jsx
+++ frontend/Paytm/src/Components/Heading.jsx
@@ -1,5 +1,5 @@
 export function Heading(props){
 return(
-    <h1 className="font-bold text-2xl text-center">{props.title}</h1>
+    <h1 className="font-bold text-2xl text-center text-red-800">{props.title}</h1>
 )
 }
