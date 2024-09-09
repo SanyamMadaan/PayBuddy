61c7054ecd74a7c17bc4bd6ef5313e3950444ba9
sanyam <sanyammadaanfca@gmail.com>
2024-09-09 21:57:35 +0530

    updated css of send money pagw

--- frontend/Paytm/src/Components/Reciever_Info.jsx
+++ frontend/Paytm/src/Components/Reciever_Info.jsx
@@ -5,9 +5,9 @@ import { useState } from "react";
 export function Reciever_Info(props){
     const [amount,setAmount]=useState(0);
     return(
-        <div className="mt-14">
-            <div className="flex">
-         <button className="border px-2 py-1 mr-2 text-center rounded-full bg-green-500">{props.name[0].toUpperCase()}</button>
+        <div className="mt-10">
+            <div className="flex mb-2">
+         <button className="border px-2 py-2 mr-2 text-center rounded-full bg-green-500">{props.name[0].toUpperCase()}</button>
          <h1 className="font-bold text-xl">{props.name.toUpperCase()}</h1>
           </div>
          <h4 className="font-medium mb-3" onChange={(e)=>setAmount(a.target.value)}>Amount (in Rs)</h4>
